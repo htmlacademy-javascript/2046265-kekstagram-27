f2191e008d95afd0360ca9cf8ab747e03acd7572
Keks <keks@htmlacademy.ru>
2022-10-21 15:41:25 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/thumbnail.js
+++ 5/js/thumbnail.js
@@ -1,7 +1,7 @@
 import {getPictures} from './data.js';
 
 const userPictures = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content.querySelector('picture');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const randomUserPictures = getPictures();
 
@@ -10,7 +10,7 @@ const pictureFragment = document.createDocumentFragment();
 randomUserPictures.forEach(({url, comments, likes}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureFragment.appendChild(pictureElement);
 });
