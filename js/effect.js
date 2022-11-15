38cfc175c1d049d1f74e92e278aee8d76cf587a2
Vadim <amirovadim30@mail.ru>
2022-11-15 10:48:36 +0300

    Добавляет изменения в модуле effect

--- js/effect.js
+++ js/effect.js
@@ -3,68 +3,63 @@ const form = document.querySelector('.img-upload__form');
 const sliderElement = document.querySelector('.effect-level__slider');
 const effectLevel = document.querySelector('.effect-level__value');
 
-const EFFECTS = [
-  {
-    name: 'none',
+const EFFECTS = {
+  none: {
     min: 0,
     max: 100,
     step: 1,
   },
-  {
-    name: 'chrome',
+  chrome: {
     style: 'grayscale',
     min: 0,
     max: 1,
     step: 0.1,
     unit: '',
   },
-  {
-    name: 'sepia',
+  sepia: {
     style: 'sepia',
     min: 0,
     max: 1,
     step: 0.1,
     unit: '',
   },
-  {
-    name: 'marvin',
+  marvin: {
     style: 'invert',
     min: 0,
     max: 100,
     step: 1,
     unit: '%',
   },
-  {
-    name: 'phobos',
+  phobos: {
     style: 'blur',
     min: 0,
     max: 3,
     step: 0.1,
     unit: 'px',
   },
-  {
-    name: 'heat',
+  heat: {
     style: 'brightness',
     min: 0,
     max: 3,
     step: 0.1,
     unit: '',
   },
-];
-const DEFAULT_EFFECT = EFFECTS[0];
+};
+const DEFAULT_EFFECT = EFFECTS.none;
 let chosenEffect = DEFAULT_EFFECT;
 
 const isDefault = () => chosenEffect === DEFAULT_EFFECT;
+const { max, min, step } = chosenEffect;
 
 const updateSlider = () => {
   sliderElement.classList.remove('hidden');
   sliderElement.noUiSlider.updateOptions({
     range: {
-      min: chosenEffect.min,
-      max: chosenEffect.max,
+      min,
+      max,
     },
-    step: chosenEffect.step,
-    start: chosenEffect.max,
+    step,
+    start: max,
   });
 
   if (isDefault()) {
@@ -76,7 +71,7 @@ const onFormChange = (evt) => {
   if (!evt.target.classList.contains('effects__radio')) {
     return;
   }
-  chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+  chosenEffect = EFFECTS[evt.target.value];
   updateSlider();
 };
 
@@ -100,11 +95,11 @@ const resetEffects = () => {
 
 noUiSlider.create(sliderElement, {
   range: {
-    min: DEFAULT_EFFECT.min,
-    max: DEFAULT_EFFECT.max,
+    min,
+    max,
   },
-  start: DEFAULT_EFFECT.max,
-  step: DEFAULT_EFFECT.step,
+  start: max,
+  step,
   connect: 'lower',
 });
 updateSlider();
